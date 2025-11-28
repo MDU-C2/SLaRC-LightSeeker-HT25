63da2c630398f4af14eabfa85716bc7c73cfefeb
eekmattias <130566117+eekmattias@users.noreply.github.com>
2025-11-28 11:28:34 +0100

    Create __init__.py

---
+++ motor_ws/s_robot/utils/motor/__init__.py
@@ -0,0 +1,21 @@
+# utils/motor/__init__.py
+
+from utils.motor import (
+    motor_api,
+    motor_group,
+    motor_odometry,
+    motor_telemetry,
+    can_bus,
+    packers,
+    motors
+)
+
+__all__ = [
+    motor_api,
+    motor_group,
+    motor_odometry,
+    motor_telemetry,
+    can_bus,
+    packers,
+    motors
+]
