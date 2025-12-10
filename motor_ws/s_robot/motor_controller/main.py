f59e4ec5616346f7a300fdc9d00c0b24d2da6bc1
eekmattias <130566117+eekmattias@users.noreply.github.com>
2025-12-10 15:21:31 +0100

    Update main.py
    Added odometry to ROS publisher

--- motor_ws/s_robot/motor_controller/main.py
+++ motor_ws/s_robot/motor_controller/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import time
 import subprocess
+import math
 
 import rclpy
 from rclpy.node import Node
@@ -30,7 +31,7 @@ def bring_down_can():
 class BatteryController(Node):
     def __init__(self):
         super().__init__("battery_controller")
-
+        
         self.batteryPublisher = self.create_publisher(BatteryInfo, "battery_info", 10)
         self.publish_timer_batt = self.create_timer(10, self.publish_battery_info)
         # Start CAN Bus
@@ -75,8 +76,15 @@ def build_motors(bus, ids):
 class MotorController(Node):
     def __init__(self):
         super().__init__("motor_controller")
-        self.create_subscription(TwistStamped, "/cmd_vel", self.receive_twist, 10)
 
+        # Odometry initializers with a track_width set arbitrarily to 0.5 m and publish it at 10 Hz
+        self.odom_pub = self.create_publisher(Odometry, "/odom", 10)
+        self.last_odom_time = time.time()
+        self.odom = SkidSteerOdometry(track_width_m=0.5)
+        self.odom_timer = self.create_timer(0.05, self.publish_odometry)
+        
+        self.create_subscription(TwistStamped, "/cmd_vel", self.receive_twist, 10)
+        
         #self.motorPublisher = self.create_publisher(MotorInfo, "motor_info", 10)
         self.publish_timer_motor = self.create_timer(10, self.ROS_telemetry)
 
@@ -105,10 +113,6 @@ class MotorController(Node):
             m.set_mode(2)
             time.sleep(0.01)
 
-        # Odometry initializers with a track_width set arbitrarily to 0.5m
-        self.odom = SkidSteerOdometry(track_width_m=0.5)
-        self.last_odom_time = time.time()
-
     def __del__(self):
         # Shutdown CAN bus
         try:
@@ -127,6 +131,44 @@ class MotorController(Node):
         #print(v_x, w_z)
         # Apply skid-steer output using Twist
         self.group.apply_twist(v_x, w_z)
+
+    def publish_odometry(self):
+        now = time.time()
+        dt = now - self.last_odom_time
+        self.last_odom_time = now
+
+        # RPM -> m/s conversion for left and right
+        v_left = self.left[0].rpm_to_speed()
+        v_right = self.right[0].rpm_to_speed()
+
+        # Update odometry state
+        x, y, yaw = self.odom.update(v_left, v_right, dt)
+
+        # Build Odometry message: timestamp it, set frames, and publish robot pose in the odom -> base_link transform
+        msg = Odometry()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "odom"
+        msg.child_frame_id = "base_link"
+
+        # UGV position
+        msg.pose.pose.position.x = x
+        msg.pose.pose.position.y = y
+        msg.pose.pose.position.z = 0.0
+
+        # UGV orientation as quaternion (yaw)
+        msg.pose.pose.orientation = Quaternion(
+            x = 0.0,
+            y = 0.0,
+            z = math.sin(yaw/2.0),
+            w = math.cos(yaw/2.0),
+        )
+
+        # UGV Velocities
+        msg.twist.twist.linear.x = (v_left + v_right) / 2.0
+        msg.twist.twist.angular.z = (v_right - v_left) / self.odom.track_width
+
+        # Publish odom
+        self.odom_pub.publish(msg)
 
     def ROS_telemetry(self: MotorTelemetry):
         self.update_from_frame()
