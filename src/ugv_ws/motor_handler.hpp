67d59444234eed70cfb7da7ee203e722228b6a4e
bergmansss <157030016+bergmansss@users.noreply.github.com>
2025-12-15 13:34:08 +0100

    Refactor Motors class and add new motor-related classes

--- src/ugv_ws/motor_handler.hpp
+++ src/ugv_ws/motor_handler.hpp
@@ -18,24 +18,61 @@ const int CMD_SET_POS_SPD = 6;
 
 const int CMD_SET_MODE = 10;
 
-
-
-
-
-// Motor
 class Motors {
     
     public:
-    Motors(std::string battery_IDm, std::string name);
+    Motors(std::string bus, std::string motor_id);
     ~Motors();
 
-    void pack_message_to_ROS(); //Returns msg -> BatteryInfo "struct"
-    int update_from_frame(int data_from_buffer); //Translates data from CAN Bus hex format to decimal format
-    bool allow_drive(); //Returns true if battery is OK
+    void set_mode(int mode);
+    void set_rpm(int rpm);
+    void stop();
+    bool handle_rx();
 
     private:
-    int decode_error(); //Sets error flags with info from CAN bus
+    
+};
+
+class MotorGroup {
+
+    public:
+    MotorGroup(std::string left, std::string right, int track_width_m, int wheel_radius_m);
+    ~MotorGroup();
+
+    float twist_to_rpm(float v_x, float w_z);
+    void apply_twist(float v_x, float w_z);
+};
+
+class MotorTelemetry {
+
+    public:
+    MotorTelemetry(int track_width_m);
+    ~MotorTelemetry();
+
+    int update_from_frame(int data);
 
 };
 
-// MotorManager
+class SkidSteerController {
+
+    public:
+    SkidSteerController(int group, int max_rpm, int turn_strength);
+    ~SkidSteerController();
+
+};
+
+class SkidSteerOdometry {
+
+    public:
+    SkidSteerOdometry(int track_width_m);
+    ~SkidSteerOdometry();
+
+    int update(float v_left, float v_right, float dt);
+
+};
+
+class packers {
+
+    public:
+    int pack_int32(int32_t value);
+};
